a34df053416a71d90319cf2dd4c8f4489dd898b6
mohitarora11 <mohit.11.arora@gmail.com>
2019-03-15 12:23:39 +0530

    news page functionality completted

--- src/body/homepage/homepage.jsx
+++ src/body/homepage/homepage.jsx
@@ -42,6 +42,80 @@ export default class Homepage extends React.Component{
 
     }
 
+    getData(){
+        let upvoteSessionData = sessionStorage.getItem("upvote");
+        if(upvoteSessionData !== null){
+            upvoteSessionData = JSON.parse(upvoteSessionData);
+        }
+        return upvoteSessionData;
+    }
+
+    storeData(id,type){
+        let upvotedata = {};
+        let checkSessionData = sessionStorage.getItem("upvote");
+        if(checkSessionData ==null){
+
+
+            let upvote = {}
+            if(type === "hide"){
+                upvotedata["hide"] = true;
+            }else{
+                upvotedata["upvoteCount"] = 1;
+            }
+            upvote[id] = upvotedata;
+            let upvoteData = JSON.stringify(upvote);
+            sessionStorage.setItem("upvote",upvoteData);
+            return;
+        }
+        else {
+            let parseData = JSON.parse(checkSessionData);
+            if(parseData[id]){
+                if(type === "hide"){
+                    parseData[id]["hide"] = true;
+                }else {
+                    parseData[id]["upvoteCount"] = Number(parseData[id]["upvoteCount"]) + 1;
+                }
+            }else{
+                if(type === "hide"){
+                    upvotedata["hide"] = true;
+                }else{
+                    upvotedata["upvoteCount"] = 1;
+                }
+
+                parseData[id]=upvotedata;
+            }
+
+            let upvoteData  = JSON.stringify(parseData);
+            sessionStorage.setItem("upvote",upvoteData);
+        }
+    }
+
+    pagination(){
+        let that = this;
+        let currentPageNumber = that.state.pageIndex+1;
+        //let pageItemIndex = that.checkCardNumbers(currentPageNumber)
+        fetch("http://hn.algolia.com/api/v1/search?page="+currentPageNumber)
+            .then((res) => res.json())
+            .then((res) => {
+                if(res) {
+                    that.updateData(res,"updatePage")
+                }
+            })
+    }
+
+    upvote(id,e){
+        //console.log(id)
+        this.storeData(id);
+        let previousValue  = window.$(e.target.previousSibling).text()
+        window.$(e.target.previousSibling).text(Number(previousValue) + 1)
+    }
+
+    hideStory(id,e){
+        this.storeData(id,"hide");
+
+        window.$(e.target).closest("#id_news").hide()
+    }
+
     render(){
         let itemindex = this.state.showingPageNumber === 0 ? this.state.showingPageNumber : this.state.showingPageNumber+10;
         return(
@@ -59,18 +133,18 @@ export default class Homepage extends React.Component{
                                                     {itemindex +index+ 1}.
                                                 </span>
                                                 <span className="hand" id="id_upvote">{item.upvoteCount}</span>
-                                                <span className="hand" >upvote</span>
+                                                <span className="hand" onClick={this.upvote.bind(this,item.objectID)}>upvote</span>
                                                 <a href={item.url}>{item.title}</a>
                                             </span>
                                             </div>
                                             <p>{item.points} points
                                                 by {item.author} {parseInt(item.created_at_i / 86400000)} hour
-                                                ago | <span className="hand" >hide</span>| <NavLink to={"/"+item.objectID}>{item.num_comments ? item.num_comments:0} comments</NavLink></p>
+                                                ago | <span className="hand" onClick={this.hideStory.bind(this,item.objectID)}>hide</span>| <NavLink to={"/"+item.objectID}>{item.num_comments ? item.num_comments:0} comments</NavLink></p>
                                         </div>)
                                 })
                                 }
 
-                                <div className="more" >More</div>
+                                <div className="more" onClick={this.pagination.bind(this)}>More</div>
 
                         </div>
                     </React.Fragment>
